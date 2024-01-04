38f3076a4d16caf58429bb747524af70801d4640
dokyu <dokyung36@naver.com>
2024-01-04 13:40:09 +0900

    first commit

---
+++ src/main/java/com/example/AI_DATA/bulletin/Service/BulletinService.java
@@ -0,0 +1,28 @@
+package com.example.AI_DATA.bulletin.Service;
+
+import com.example.AI_DATA.bulletin.repository.BulletinRepository;
+import com.example.AI_DATA.bulletin.model.Bulletin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class BulletinService {
+    private final BulletinRepository bulletinRepository;
+
+    @Autowired
+    public BulletinService(BulletinRepository bulletinRepository) {
+        this.bulletinRepository = bulletinRepository;
+    }
+
+    public void save(Bulletin bulletin) {
+        this.bulletinRepository.save(bulletin);
+    }
+
+    public Optional<Bulletin> findByid(Long id) {
+        return this.bulletinRepository.findById(id);
+    }
+
+}
