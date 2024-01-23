01fc46a8f92d882c28059229ee38ccb149aa47ab
dokyu <dokyung36@naver.com>
2024-01-23 18:02:51 +0900

    logout finished, test code implementation need

--- src/main/java/com/example/AI_DATA/user/model/User.java
+++ src/main/java/com/example/AI_DATA/user/model/User.java
@@ -2,10 +2,12 @@ package com.example.AI_DATA.user.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.AllArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "user")
+@AllArgsConstructor
 public class User {
     @Id
     @Column(name=  "id")
