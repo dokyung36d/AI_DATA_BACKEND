a7780cf96f0cdd4c2a7516c9f2139f02954f5983
dokyu <dokyung36@naver.com>
2024-02-13 16:48:05 +0900

    need to write request service to AI-server

--- src/main/java/com/example/AI_DATA/bulletin/Service/BulletinService.java
+++ src/main/java/com/example/AI_DATA/bulletin/Service/BulletinService.java
@@ -1,17 +1,32 @@
 package com.example.AI_DATA.bulletin.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.FileSystemResource;
 import com.example.AI_DATA.bulletin.repository.BulletinRepository;
 import com.example.AI_DATA.bulletin.model.Bulletin;
+import com.fasterxml.jackson.core.type.TypeReference;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpEntity;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
 
+import java.io.File;
 import java.util.*;
 
 @Service
 public class BulletinService {
     private final BulletinRepository bulletinRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     public BulletinService(BulletinRepository bulletinRepository) {
@@ -29,6 +44,28 @@ public class BulletinService {
     public long getRowNumber() { return this.bulletinRepository.countRows(); }
 
     public long getLatestBulletinId() { return this.bulletinRepository.getLatestBulletinId(); }
+
+    public void sendRequestToAIServer(String imagePath) {
+        String url = "http://localhost:8081//AICOSS/image/prediction";
+        File jpgFile = new File(imagePath);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        FileSystemResource fileSystemResource = new FileSystemResource(jpgFile);
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("jpgFile", fileSystemResource);
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
+
+        if (response.getStatusCode().is2xxSuccessful()) {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            Map<String, String> resultMap = objectMapper.readValue(response.getBody(), new TypeReference<Map<String, Object>>() {});
+        }
+    }
 
 
 
