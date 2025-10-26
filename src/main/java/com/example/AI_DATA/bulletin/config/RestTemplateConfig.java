9556a99f283b51c56872a1f1c1deceb4fcd898b7
dokyu <dokyung36@naver.com>
2025-10-26 12:07:20 +0900

    REFACTOR: replaced wildcard imports with explicit imports

--- src/main/java/com/example/AI_DATA/bulletin/config/RestTemplateConfig.java
+++ src/main/java/com/example/AI_DATA/bulletin/config/RestTemplateConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
 @Configuration
 public class RestTemplateConfig {
     @Bean
